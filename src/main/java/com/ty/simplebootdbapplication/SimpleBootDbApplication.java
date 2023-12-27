98b4b2cd49f85d340bcba2cb57e21aeeec7110ab
mukund-waghmare <mukundwaghmare4950@gmail.com>
2023-12-27 17:00:54 +0530

    first-commit

---
+++ src/main/java/com/ty/simplebootdbapplication/SimpleBootDbApplication.java
@@ -0,0 +1,13 @@
+package com.ty.simplebootdbapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBootDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleBootDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ty/simplebootdbapplication/SimpleBootDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ty.simplebootdbapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBootDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
